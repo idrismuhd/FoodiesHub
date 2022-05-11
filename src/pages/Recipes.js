1b0e951ce68288951224a32a62daf56c2989bce0
idrismuhd <46567163+idrismuhd@users.noreply.github.com>
2022-05-11 21:40:07 +0100

    recipes updates

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -6,62 +6,62 @@ export default function Recipes() {
     {
       title: 'Chicken Pan Pizza',
       image: '/img/gallery/img_1.jpg',
-      authorImg: '/img/top-chiefs/img_1.jpg',
+      authorImg: '/img/top-chiefs/download (2).jpg',
     },
     {
       title: 'Spaghetti and Meatballs',
       image: '/img/gallery/img_4.jpg',
-      authorImg: '/img/top-chiefs/img_2.jpg',
+      authorImg: '/img/top-chiefs/download (3).jpg',
     },
     {
       title: 'American Cheese Burger',
       image: '/img/gallery/img_5.jpg',
-      authorImg: '/img/top-chiefs/img_3.jpg',
+      authorImg: '/img/top-chiefs/download (4).jpg',
     },
     {
       title: 'Mutton Biriyani',
       image: '/img/gallery/img_6.jpg',
-      authorImg: '/img/top-chiefs/img_5.jpg',
+      authorImg: '/img/top-chiefs/download (5).jpg',
     },
     {
       title: 'Japanese Sushi',
       image: '/img/gallery/img_10.jpg',
-      authorImg: '/img/top-chiefs/img_6.jpg',
+      authorImg: '/img/top-chiefs/download.jpg',
     },
     {
       title: 'Chicken Pan Pizza',
       image: '/img/gallery/img_1.jpg',
-      authorImg: '/img/top-chiefs/img_1.jpg',
+      authorImg: '/img/top-chiefs/download (1).jpg',
     },
     {
       title: 'Spaghetti and Meatballs',
       image: '/img/gallery/img_4.jpg',
-      authorImg: '/img/top-chiefs/img_2.jpg',
+      authorImg: '/img/top-chiefs/download (2).jpg',
     },
     {
       title: 'American Cheese Burger',
       image: '/img/gallery/img_5.jpg',
-      authorImg: '/img/top-chiefs/img_3.jpg',
+      authorImg: '/img/top-chiefs/download (3).jpg',
     },
     {
       title: 'Mutton Biriyani',
       image: '/img/gallery/img_6.jpg',
-      authorImg: '/img/top-chiefs/img_5.jpg',
+      authorImg: '/img/top-chiefs/download (5).jpg',
     },
     {
       title: 'Japanese Sushi',
       image: '/img/gallery/img_10.jpg',
-      authorImg: '/img/top-chiefs/img_6.jpg',
+      authorImg: '/img/top-chiefs/download.jpg',
     },
     {
       title: 'Chicken Pan Pizza',
       image: '/img/gallery/img_1.jpg',
-      authorImg: '/img/top-chiefs/img_1.jpg',
+      authorImg: '/img/top-chiefs/download (2).jpg',
     },
     {
       title: 'Mutton Biriyani',
       image: '/img/gallery/img_6.jpg',
-      authorImg: '/img/top-chiefs/img_5.jpg',
+      authorImg: '/img/top-chiefs/download (3).jpg',
     },
   ].sort(() => Math.random() - 0.5)
 
